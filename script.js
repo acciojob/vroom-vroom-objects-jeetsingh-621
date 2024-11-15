624696ed9c5f9a8b4b71b73aa166274b2e30c825
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-15 10:54:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ function Car(make, model) {
 	this.make = make;
 	this.model = model;
 }
-car.prototype.getMakeModel=function()=>{
-	return `${this.make} ${this.model}`
-}
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
 
 
 function SportsCar(make, model, topSpeed) {
-	car.call(this,make,model);
-	this.topSpeed=topSpeed;
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
-SportsCar.prototype=Object.create(car.prototype);
+SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
+
 SportsCar.prototype.getTopSpeed = function() {
   return this.topSpeed;
 };
